cbb89239b54af0b0832b401c31de0001b12e6d58
Hlib Sobolevskyi <volsoor@gmail.com>
2024-06-11 12:22:13 -0700

    Initial Commit

---
+++ FetchRecipes/FetchRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipesApp.swift
+//  FetchRecipes
+//
+//  Created by Hlib Sobolevskyi on 2024-06-11.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
